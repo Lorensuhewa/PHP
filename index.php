4c5dd6ea0aafd9a23697c5b369da5f1c8c9d3537
Sanaka Lorensuhewa <153020684+Lorensuhewa@users.noreply.github.com>
2025-05-12 17:25:57 +0530

    welcome php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    echo "I love PHP! <br>";
+    echo "It's a great language for web development. <br>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>Order pitzza</button>
+</body>
+</html>
