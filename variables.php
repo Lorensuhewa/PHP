9d0beeea9b67c175eec6ee0423fb902b89344a7e
Sanaka Lorensuhewa <153020684+Lorensuhewa@users.noreply.github.com>
2025-05-12 17:42:03 +0530

    variables

---
+++ variables.php
@@ -0,0 +1,41 @@
+<?php 
+// Variables in PHP
+
+// String
+
+$name = "Sanaka Lorensuhewa";
+$university = "University of Ruhuna";
+
+// Intiger
+
+$age = 25;
+$year = 3;
+
+// Float
+
+$height = 5.9;
+$weight = 70.5;
+
+//Double 
+
+$Salery = 30_000.00;
+
+
+// Boolean
+
+$isMarried = false;
+$isStudent = true;
+
+//output
+
+echo "Hello {$name}. You are from {$university}.<br>";
+echo "Your year is {$year}.<br>";
+echo "You are {$age} years old.<br>";
+echo "Your height is {$height}.<br>";
+echo "Your weight is {$weight}.<br>";
+echo "Your salery is {$Salery}.<br>";
+echo "Your marital status is {$isMarried}.<br>";
+echo "Your student status is {$isStudent}.<br>";
+
+
+?>
