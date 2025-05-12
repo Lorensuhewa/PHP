11b752d63b0de9e7008ee4bf92cd104ed23bb1e4
Sanaka Lorensuhewa <153020684+Lorensuhewa@users.noreply.github.com>
2025-05-12 18:06:03 +0530

    operations

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php
     echo "Hello <br>";
     echo "Welcome sanaka Lorensuhewa PHP lessons <br>";
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Welcome to PHP</h1>
-</body>
-</html>
+?>
---
+++ operations.php
@@ -0,0 +1,55 @@
+<?php 
+
+// Arithmetic Operations
+// + - * / ** %
+
+// Addition
+$a = 10;
+$b = 5;
+
+$sum = $a + $b;
+echo "Sum: " . $sum . "<br>";
+
+// Subtraction
+$subtraction = $a - $b;
+echo "Subtraction: " . $subtraction . "<br>";
+
+// Multiplication
+$multiplication = $a * $b;
+echo "Multiplication: " . $multiplication . "<br>";
+
+// Division
+$division = $a / $b;
+echo "Division: " . $division . "<br>";
+
+//power
+$power = $a ** $b;
+echo "Power: " . $power . "<br>";
+
+// Modulus
+$modulus = $a % $b;
+echo "Modulus: " . $modulus . "<br>";
+
+// Increment and decrement operations
+
+$x = 5;
+
+//increment
+$x++;
+
+echo $x;
+
+//decrement
+$x--;
+
+echo $x;
+
+//operator Precedence
+
+// () 
+// **
+// * / %
+// + -
+
+
+?>
